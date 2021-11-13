8413d889db67e3d3e438592ab50e813c2c655f2b
JahazielHernandezHoyos <48532611+JahazielHernandezHoyos@users.noreply.github.com>
2021-11-12 18:46:14 -0600

    Add files via upload

---
+++ Apps/Academica/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from Apps.Academica.models import *
+
+# Register your models here.
+
+admin.site.register(Alumno)
+admin.site.register(Curso)
+admin.site.register(Matricula)
